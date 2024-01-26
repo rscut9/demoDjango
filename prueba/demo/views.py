1eb95cfc0430a61e066158538f71c7cb126277d0
Robert Scutaru <robert.scutaru@unir.net>
2024-01-26 13:29:49 +0100

    comentarios añadidos

--- prueba/demo/views.py
+++ prueba/demo/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-
-from demo.utils.mongo_connection import MongoConn
-
 from pymongo import MongoClient
 
-# Vistas de la API
-
-from django.shortcuts import render
-from pymongo import MongoClient
-
+# Vista para mostrar los detalles de una colección
 def mostrar_coleccion(request):
     try:
+        # Conexión a la base de datos MongoDB
         client = MongoClient('localhost', 27017)
         mydatabase = client.dumpmongo
 
@@ -29,19 +23,29 @@ def mostrar_coleccion(request):
                 return render(request, 'detalle_coleccion.html', {'coleccion': selected_collection, 'datos': lista_datos})
             else:
                 mensaje = 'Colección no válida'
+                # Redirige a la página 'ver_coleccion' con un mensaje de error
                 return render(request, 'ver_coleccion.html', {'colecciones': mydatabase.list_collection_names(), 'mensaje': mensaje})
 
     except Exception as e:
+        # Maneja cualquier excepción e imprime el mensaje de error
         print(f"Error: {e}")
+        # Redirige a la página 'error_template' con el mensaje de error
         return render(request, 'error_template.html', {'error_message': str(e)})
 
+# Vista para mostrar la lista de colecciones disponibles
 def ver_coleccion(request):
     try:
+        # Conexión a la base de datos MongoDB
         client = MongoClient('localhost', 27017)
         mydatabase = client.dumpmongo
+
+        # Obtiene la lista de colecciones y las ordena alfabéticamente
         colecciones = sorted(mydatabase.list_collection_names())
 
+        # Renderiza la página 'ver_coleccion' con la lista de colecciones
         return render(request, 'ver_coleccion.html', {'colecciones': colecciones})
     except Exception as e:
+        # Maneja cualquier excepción e imprime el mensaje de error
         print(f"Error: {e}")
+        # Redirige a la página 'error_template' con el mensaje de error
         return render(request, 'error_template.html', {'error_message': str(e)})
