a1587b640fbfb44713bd9cb33426d2e7bebd4dd5
Robert Scutaru <robert.scutaru@unir.net>
2024-02-09 13:25:28 +0100

    implementación sistema gestor de errores

--- prueba/demo/urls.py
+++ prueba/demo/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 from .views import *
 
-#def trigger_error(request):
-#    division_by_zero = 1 / 0
-
 urlpatterns = [
     path('', home, name='home'),
     path('logout/', exit, name='exit'),
     path('ver/', ver_coleccion, name='ver_coleccion'),
     path('mostrar/', mostrar_coleccion, name='mostrar_coleccion'),
     path('crear_usuario/', create_user, name='crear_usuario'),
-
-    #path('sentry-debug/', trigger_error),
-    
 ]
